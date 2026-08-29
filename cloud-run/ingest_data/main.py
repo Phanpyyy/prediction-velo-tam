b67c2f4140f7ab651dc16a6fd9d40daeabaa3505
Phanpy <foutelromane@gmail.com>
2026-08-29 17:01:39 +0200

    uv

--- cloud-run/ingest_data/main.py
+++ cloud-run/ingest_data/main.py
@@ -15,7 +15,6 @@ PARIS_TZ = ZoneInfo("Europe/Paris")
 
 @functions_framework.http
 def ingest_realtime_data(request):
-    """Point d'entrée de la Cloud Function exécuté par Cloud Run."""
     url = "https://portail-api-data.montpellier.fr/ngsi-ld/v1/entities?type=BikeHireDockingStation"
 
     try:
@@ -31,18 +30,14 @@ def ingest_realtime_data(request):
         for entity in entities:
             full_id = entity.get("id", "")
             station_id = full_id.split(":")[-1] if ":" in full_id else full_id
-            # On applique zfill(3) pour la cohérence des ID avec le référentiel
             station_id = str(station_id).zfill(3)
 
-            available_bike = entity.get("availableBikeNumber", {}).get(
-                "value", 0
-            )
+            available_bike = entity.get("availableBikeNumber", {}).get("value", 0)
             free_slots = entity.get("freeSlotNumber", {}).get("value", 0)
             status = entity.get("status", {}).get("value", "Working")
 
             date_obs = entity.get("observationDateTime", {}).get("value", None)
             if date_obs:
-                # La date de l'API est en UTC -> conversion en heure de Paris
                 date_obs = pd.to_datetime(date_obs, utc=True).astimezone(PARIS_TZ)
             else:
                 date_obs = now
@@ -64,7 +59,7 @@ def ingest_realtime_data(request):
         df_latest = pd.DataFrame(data_clean)
         df_latest["date"] = pd.to_datetime(df_latest["date"])
 
-        # Envoi dans BigQuery
+        #Envoi dans bigquery
         client = bigquery.Client(project=PROJECT_ID)
         table_full_path = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
 
