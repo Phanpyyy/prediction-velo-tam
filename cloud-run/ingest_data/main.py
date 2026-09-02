7a0600d2087ce2d9cad1f6c41884bb01b8c9a0d8
Phanpy <foutelromane@gmail.com>
2026-09-02 14:03:34 +0200

    update ingest_data

--- cloud-run/ingest_data/main.py
+++ cloud-run/ingest_data/main.py
@@ -5,12 +5,17 @@ from google.cloud import bigquery
 import pandas as pd
 import requests
 
-# Constantes du projet BigQuery
+"""
+Code du cloud run pour récupérer les données en temps réel 
+(avec maj toutes les heures grâce à cloud schedule)
+Les données historiques et les données de localisation des stations sont
+à récupérer avec scripts/backfill_raw_data.py
+"""
+
+#id du projet bigquery
 PROJECT_ID = "prediction-velo"
 DATASET_ID = "prediction_velo_raw"
 TABLE_ID = "realtime"
-
-PARIS_TZ = ZoneInfo("Europe/Paris")
 
 
 @functions_framework.http
@@ -18,58 +23,59 @@ def ingest_realtime_data(request):
     url = "https://portail-api-data.montpellier.fr/ngsi-ld/v1/entities?type=BikeHireDockingStation"
 
     try:
+        #Envoie requête à l'API pour récup les données
         response = requests.get(url, timeout=10)
 
-        if response.status_code != 200:
-            return f"Erreur HTTP API : {response.status_code}", 500
+        if not response.ok:
+            return f"Erreur HTTP API : {response.status_code}", 502
 
+        #Récupération des données dans entities
         entities = response.json()
         data_clean = []
-        now = datetime.now(timezone.utc).astimezone(PARIS_TZ)
+        now = datetime.now(timezone.utc).astimezone(ZoneInfo("Europe/Paris"))
 
+        #On prend les données qui nous intéressent et on les met en forme
         for entity in entities:
-            full_id = entity.get("id", "")
-            station_id = full_id.split(":")[-1] if ":" in full_id else full_id
-            station_id = str(station_id).zfill(3)
+            #Récupération de l'id de la station qui est d'origine sous forme : "urn:ngsi-ld:station:001"
+            station_id = entity.get("id", "").split(":")[-1].zfill(3)
 
+            #Récupération du nb de vélo, du nb de places libres et du statut de la station
             available_bike = entity.get("availableBikeNumber", {}).get("value", 0)
             free_slots = entity.get("freeSlotNumber", {}).get("value", 0)
             status = entity.get("status", {}).get("value", "Working")
 
-            date_obs = entity.get("observationDateTime", {}).get("value", None)
+            #Récupération de la date et conversion de la date en timestamp
+            date_obs = entity.get("freeSlotNumber", {}).get("observedAt", None)
             if date_obs:
-                date_obs = pd.to_datetime(date_obs, utc=True).astimezone(PARIS_TZ)
+                date_obs = pd.to_datetime(date_obs, utc=True).astimezone(ZoneInfo("Europe/Paris"))
             else:
                 date_obs = now
 
             data_clean.append(
                 {
                     "station_id": station_id,
-                    "availableBikeNumber": int(available_bike)
-                    if available_bike is not None
-                    else 0,
-                    "freeSlotNumber": int(free_slots)
-                    if free_slots is not None
-                    else 0,
+                    "availableBikeNumber": int(available_bike),
+                    "freeSlotNumber": int(free_slots),
                     "status": str(status),
                     "date": date_obs,
                 }
             )
 
+        #Conversion de la liste en dataframe
         df_latest = pd.DataFrame(data_clean)
-        df_latest["date"] = pd.to_datetime(df_latest["date"])
 
         #Envoi dans bigquery
         client = bigquery.Client(project=PROJECT_ID)
         table_full_path = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
 
         job_config = bigquery.LoadJobConfig(
-            write_disposition="WRITE_APPEND",
+            write_disposition="WRITE_APPEND", #les nouvelles lignes sont ajoutées à celles existantes
             schema_update_options=[
                 bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION
             ],
         )
 
+        #Envoi des données
         job = client.load_table_from_dataframe(
             df_latest, table_full_path, job_config=job_config
         )
